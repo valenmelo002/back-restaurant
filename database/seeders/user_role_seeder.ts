193a8518a8952f1d7aac9ef39dfde985f04f9726
DiaMar15 <diamarcelaleal23@gmail.com>
2025-06-18 00:43:52 -0500

    arreglando el seeder de usuarios

--- database/seeders/user_role_seeder.ts
+++ database/seeders/user_role_seeder.ts
@@ -10,7 +10,7 @@ export default class extends BaseSeeder {
       },
       {
         user_id: 2,
-        role_id: 2,
+        role_id: 1,
       },
       {
         user_id: 3,
@@ -29,12 +29,8 @@ export default class extends BaseSeeder {
         role_id: 2,
       },
       {
-        user_id: 1,
-        role_id: 2,
-      },
-      {
         user_id: 6,
-        role_id: 1,
+        role_id: 3,
       },
     ])
   }
